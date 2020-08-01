ca781b77ee3e81e1020d909a21ae68000e883451
Curt Grogan <curtgrogan@outlook.com>
2020-08-01 08:27:16 -0400

    First get implemented and functional.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { PostsComponent } from './posts/posts.component';
+import { AppComponent } from "./app.component";
+import { PostsComponent } from "./posts/posts.component";
+import { PostsComponentComponent } from "./posts-component/posts-component.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PostsComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, PostsComponent, PostsComponentComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 @Component({
-  selector: 'app-posts',
-  templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.scss']
+  selector: "posts",
+  templateUrl: "./posts.component.html",
 })
-export class PostsComponent implements OnInit {
+export class PostsComponent {
+  posts: any[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private http: HttpClient) {
+    http
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .subscribe((response) => {
+        // this.posts = response; //works but gives warning in VS Code about the action
+        this.posts = response as any;
+        console.log(this.posts);
+      });
   }
-
+  createPost(title: HTMLInputElement) {}
 }
